2f5f11f6ea32fcc05bf0a6bb3ac1761ff0b947d4
45676f725a75796576 <egor.zuyev33@gmail.com>
2025-02-20 19:10:44 +0100

    initial commit

--- pv-db-project/DBManager.cs
+++ pv-db-project/DBManager.cs
@@ -2,20 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace pv_db_project
 {
     internal class DBManager
     {
-        private List<DBTable> usedDatabase;
-        public void Connect(string server, string login, string password)
-        {
+        private SqlConnectionStringBuilder consStringBuilder;
+        private SqlConnection connection;
 
+        //[0] table is used to get metadata
+        //[1] table is used to get all databases on server
+        //[2] table is used to get all the tables in database
+        //every other tables will load later
+        private List<DataTable> usedDatabase = new List<DataTable>(3);
+
+        public void Connect(string server, string db_name, string login, string password)
+        {
+            consStringBuilder = new SqlConnectionStringBuilder();
+            consStringBuilder.DataSource = server;
+            consStringBuilder.InitialCatalog = db_name;
+            consStringBuilder.UserID = login;
+            consStringBuilder.Password = password;
+            consStringBuilder.ConnectTimeout = 30;
+            connection = new SqlConnection(consStringBuilder.ConnectionString);
+            connection.Open();
+            usedDatabase[0] = connection.GetSchema("MetaDataCollections");
+            usedDatabase[1] = connection.GetSchema("Databases");
+            usedDatabase[2] = connection.GetSchema("Tables");
         }
-        public void Disconnect() 
+        public void Disconnect()
         {
-
+            connection.Close();
+        }
+        public DataTable GetTablesInDB() { return usedDatabase[2]; }
+        public void DeleteDBSchemeFromProgram()
+        {
+            usedDatabase = null;
         }
         public List<DBTable> GetDatabase()
         {
