0ce5178cfc698463b14b988dcfbb563629545981
zuyev <zuyev@PC1058>
2025-02-20 15:34:34 +0100

    Add project files.

---
+++ pv-db-project/DBManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv_db_project
+{
+    internal class DBManager
+    {
+        private List<DBTable> usedDatabase;
+        public void Connect(string server, string login, string password)
+        {
+
+        }
+        public void Disconnect() 
+        {
+
+        }
+        public List<DBTable> GetDatabase()
+        {
+            return usedDatabase;
+        }
+        public DBTable GetTable(string tableName)
+        {
+            foreach (DBTable t in usedDatabase)
+            {
+                if(t.table_name.Equals(tableName)) return t;
+            }
+
+            throw new Exception("Table with this name doesn't exist.");
+        }
+    }
+}
---
+++ pv-db-project/DBTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv_db_project
+{
+    internal class DBTable
+    {
+        public string table_name { get; }
+        private List<Object> attributes;
+
+        public DBTable()
+        {
+            attributes = new List<Object>();
+        }
+    }
+
+    internal class Attribute<T>
+    {
+        private T type;
+
+        public string name { get; }
+        public List<T> Records { get; }
+
+        public Attribute(T type, string name)
+        {
+            this.type = type;
+            this.name = name;
+            this.Records = new List<T>();
+        }
+    }
+}
+
