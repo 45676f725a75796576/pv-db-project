1e4bc483d5e303ed762c4cb72e867ab82d8d0c3f
45676f725a75796576 <egor.zuyev33@gmail.com>
2025-02-21 18:51:10 +0100

    addded comments

--- pv-db-project/DBManager.cs
+++ pv-db-project/DBManager.cs
@@ -12,20 +12,23 @@ namespace pv_db_project
         private SqlConnectionStringBuilder consStringBuilder;
         private SqlConnection connection;
 
+        // empty constructor
         public DBManager() { }
 
+        // constructor with connection parameters and automatic connection
         public DBManager(string server, string db_name, string login, string password)
         {
             Connect(server, db_name, login, password);
         }
 
+        // set connection parameters and open connection to the database
         public void Connect(string server, string db_name, string login, string password)
         {
             consStringBuilder = new SqlConnectionStringBuilder();
             consStringBuilder.DataSource = server;
             consStringBuilder.InitialCatalog = db_name;
-            //consStringBuilder.UserID = login;
-            //consStringBuilder.Password = password;
+            consStringBuilder.UserID = login;
+            consStringBuilder.Password = password;
             consStringBuilder.ConnectTimeout = 30;
             consStringBuilder.TrustServerCertificate = true;
             consStringBuilder.IntegratedSecurity = true;
@@ -33,8 +36,10 @@ namespace pv_db_project
             connection.Open();
         }
 
+        // returns the connection object
         public SqlConnection Connection { get { return connection; } }
 
+        // close the connection
         public void Dispose()
         {
             connection.Close();
--- pv-db-project/DatabaseObjects/IDBObject.cs
+++ pv-db-project/DatabaseObjects/IDBObject.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace CsharpDatabaseAPI.DatabaseObjects
 {
+    // Interface for all database objects
     interface IDBObject
     {
+        // Add a new record to the table
         public static void Add (string column_name, string input, SqlConnection conn) { }
+        // Delete a record from the table, column_name is the column to filter, if its value is equal to filter, the record is deleted
         public static void DeleteRecord (string column_name, string filter, SqlConnection conn) { }
+        // Get all values from a column, input is a column name
         public static string[] GetColumn (string input, SqlConnection conn) { return null; }
+        // Find all records where column_name is equal to filter
         public static string[] FindByColumn (string column_name, string filter, SqlConnection conn) { return null; }
+        // Update a record in the table, f_column_name is the column to filter, if its value is equal to filter, the record is updated to a new_value
         public static void Update (string f_column_name, string filter, string n_column_name, string new_value, SqlConnection conn) { }
     }
 }
